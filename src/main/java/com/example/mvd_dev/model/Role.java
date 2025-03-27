cbb9e3d43acf0c5ee132985451452d2af0028dbf
WISP <144270275+MatveyTolstov@users.noreply.github.com>
2025-03-27 19:18:29 +0300

    Создал проект

---
+++ src/main/java/com/example/mvd_dev/model/Role.java
@@ -0,0 +1,15 @@
+package com.example.mvd_dev.model;
+
+//import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Entity
+@Setter
+@Getter
+public class Role {
+
+    private int ID_Role;
+
+    private String RoleName;
+}
